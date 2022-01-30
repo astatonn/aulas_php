2c795f0565104d98d1c868abe95e97b69c62bed9
Lucas Lima <lucas.lima.rk@gmail.com>
2022-01-30 14:48:56 -0300

    PHP 8 #014 EXERCÍCIOS COM OPERADORES ARITIMÉTICOS E DE ATRIBUIÇÃO

--- aula_01.php
+++ aula_01.php
@@ -1,3 +1,42 @@
 <?php
 
-?>
+echo 'From: https://www.youtube.com/watch?v=OdLkcq5SC5U&list=PLXik_5Br-zO9wODVI0j58VuZXkITMf7gZ&index=14';
+echo '<br>';
+echo '<br>';
+echo '<br>';
+
+/**********************EXERCÍCIO 1************************/
+echo 'Exercício 1';
+echo '<br>';
+$a = 1000;
+$b = $a/2;
+echo $b;
+echo '<br>';
+
+/**********************EXERCÍCIO 2************************/
+echo '<br>';
+echo 'Exercício 2';
+echo '<br>';
+$a = 100;
+echo $b*3;
+echo '<br>';
+
+/**********************EXERCÍCIO 3************************/
+echo '<br>';
+echo 'Exercício 3';
+echo '<br>';
+$a = 20;
+echo 'Valor Atual: '.$a.'<br>';
+echo 'Valor Atualizado: '.++$a;
+echo '<br>';
+
+/**********************EXERCÍCIO 4************************/
+echo '<br>';
+echo 'Exercício 4';
+echo '<br>';
+$a = 100;
+$b = 250;
+$c = $a + $b;
+$c++;
+echo 'Resultado será: '.$c;
+echo '<br>';
